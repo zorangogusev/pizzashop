70d486f51eb35f3cb68e5e2e855d9a4e5ff07ae1
zoran <zoran@debian-zoran.debian10.org>
2020-09-13 12:44:21 +0200

    First commit

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,13 @@
+@extends ('front.layouts.master')
+@section ('title','Home Page')
+@section ('content')
+    <section>
+        <div class="container">
+            <div class="row">
+                    test
+
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/front/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>@yield('title','Master Page')</title>
+    <link rel="stylesheet" href="{{ asset('front/fontawesome/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/css/responsive.css') }}">
+</head><!--/head-->
+
+<body>
+@include('front.layouts.header')
+
+@yield('content')
+
+@include('front.layouts.footer')
+<script src="{{ asset('front/js/jquery.min.js') }}"></script>
+<script src="{{ asset('front/fontawesome/js/all.js') }}"></script>
+<script src="{{ asset('front/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('front/js/main.js') }}"></script>
+</body>
+</html>
