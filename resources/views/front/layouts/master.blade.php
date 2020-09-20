35a86b7e4a1cc530adf742842667263eb3dca1c7
zoran_gogusev <zoran_gogusev@yahoo.com>
2020-09-20 16:39:42 +0200

    Deploying on Heroku

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -5,6 +5,9 @@
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
     <meta content="utf-8" http-equiv="encoding">
 
+
+    <meta content="Authorization: Bearer 1a06bac3-3d64-4861-9cca-9f998e7877d1">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
